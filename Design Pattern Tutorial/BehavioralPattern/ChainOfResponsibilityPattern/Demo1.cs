008f3bae63be1e6891c19c33854f63cb1eb43d18
kashuvo <shuvo_CSE23@yahoo.com>
2020-06-01 02:34:47 +0600

    Chain of responsibility

---
+++ Design Pattern Tutorial/BehavioralPattern/ChainOfResponsibilityPattern/Demo1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace ChainOfResponsibilityPattern
+{
+    public class Creature
+    {
+        public string Name;
+        public int Attack, Defence;
+        public Creature(string Name, int Attack, int Defence)
+        {
+            this.Name = Name;
+            this.Attack = Attack;
+            this.Defence = Defence;
+        }
+
+        public override string ToString()
+        {
+            return $"Name {Name}, Attack {Attack}, Defence {Defence}";
+        }
+    }
+
+    public class CreatureModifier
+    {
+        protected Creature creature;
+        protected CreatureModifier next;
+
+        public CreatureModifier(Creature creature)
+        {
+            this.creature = creature;
+        }
+
+        public void Add(CreatureModifier cm) // Creating the chain.
+        {
+            if (next != null) next.Add(cm);
+            else next = cm;
+        }
+
+        public virtual void Handle()
+        {
+            next?.Handle();
+        }
+    }
+
+    public class AttackModifier : CreatureModifier
+    {
+        public AttackModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {creature.Name}'s attack");
+            creature.Attack *= 2;
+            base.Handle();
+        }
+    }
+
+    public class DefenceModifier : CreatureModifier
+    {
+        public DefenceModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Increasing {creature.Name}'s defence");
+            creature.Defence += 3;
+            base.Handle();
+        }
+    }
+
+    public class NoModifierSpell : CreatureModifier
+    {
+        public NoModifierSpell(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine("Modification is turned off by a curse spell.");
+            // Do nothing.
+        }
+    }
+
+    class Demo1
+    {
+        static void Main(string[] args)
+        {
+            var goblin = new Creature("Goblin", 2, 2);
+            Console.WriteLine(goblin);
+
+            var root = new CreatureModifier(goblin);
+            root.Add(new NoModifierSpell(goblin));    // Stops modification on creature.
+
+            root.Add(new AttackModifier(goblin));
+            root.Add(new DefenceModifier(goblin));
+
+            root.Handle();
+
+            Console.WriteLine(goblin);
+        }
+    }
+}
---
+++ Design Pattern Tutorial/BehavioralPattern/ChainOfResponsibilityPattern/Demo2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ChainOfResponsibilityPattern.EnumHolder;
+
+namespace ChainOfResponsibilityPattern
+{
+    public class EnumHolder
+    {
+        public enum OperationType
+        {
+            Add,
+            Sub,
+            Mul
+        }
+    }
+
+    public class Numbers
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public OperationType operationType;
+
+        public Numbers(int Num1, int Num2, OperationType operationType)
+        {
+            this.Num1 = Num1;
+            this.Num2 = Num2;
+            this.operationType = operationType;
+        }
+    }
+
+    public class Chain
+    {
+        protected Chain next;
+        public Chain AddToChain(Chain node)
+        {
+            if (next == null)
+                next = node;
+            else
+                next.AddToChain(node);
+
+            return this;
+        }
+
+        public virtual string Calculate(Numbers numbers)
+        {
+            return next?.Calculate(numbers);
+        }
+    }
+
+    public class Addition : Chain
+    {
+        public override string Calculate(Numbers numbers)
+        {
+            if (numbers.operationType == OperationType.Add)
+            {
+                int res = numbers.Num1 + numbers.Num2;
+                return $"{numbers.Num1} + {numbers.Num2} = {res}";
+            }
+            else
+            {
+                if (next == null)
+                    return "OperationType is unrecognized";
+                else
+                    return next.Calculate(numbers);
+            }
+        }
+    }
+
+    public class Subtraction : Chain
+    {
+        public override string Calculate(Numbers numbers)
+        {
+            if (numbers.operationType == OperationType.Sub)
+            {
+                int res = numbers.Num1 - numbers.Num2;
+                return $"{numbers.Num1} - {numbers.Num2} = {res}";
+            }
+            else
+            {
+                if (next == null)
+                    return "OperationType is unrecognized";
+                else
+                    return next.Calculate(numbers);
+            }
+        }
+    }
+
+    public class Multiplication : Chain
+    {
+        public override string Calculate(Numbers numbers)
+        {
+            if (numbers.operationType == OperationType.Mul)
+            {
+                int res = numbers.Num1 * numbers.Num2;
+                return $"{numbers.Num1} * {numbers.Num2} = {res}";
+            }
+            else
+            {
+                if (next == null)
+                    return "OperationType is unrecognized";
+                else
+                    return next.Calculate(numbers);
+            }
+        }
+    }
+
+    public class Demo2
+    {
+        static void Main(string[] args)
+        {
+            var chain = new Chain();
+            chain.AddToChain(new Addition())
+                .AddToChain(new Subtraction())
+                .AddToChain(new Multiplication());
+
+            Numbers numbers = new Numbers(5, 10, OperationType.Mul);
+            var result = chain.Calculate(numbers);
+            Console.WriteLine(result);
+        }
+    }
+}
